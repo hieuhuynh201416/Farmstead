0183b30aaf5d40c909e1309ef211cf0dd4873ee4
KHA0410 <khanguyen151121@gmail.com>
2023-09-17 19:52:14 +0700

    Thêm back to top

---
+++ index.js
@@ -0,0 +1,26 @@
+var scrollToTopButton = document.getElementById("movetop");
+
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopButton.style.display = "block";
+    } else {
+        scrollToTopButton.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    var currentPosition = document.documentElement.scrollTop || document.body.scrollTop;
+    
+    if (currentPosition > 0) {
+        window.requestAnimationFrame(scrollToTop);
+        window.scrollTo(0, currentPosition - 50); // Điều chỉnh giá trị /8 để làm cho cuộn mượt hơn
+    }
+}
+
+scrollToTopButton.addEventListener("click", function() {
+    scrollToTop();
+});
